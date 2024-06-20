884f30bea5ea84d583ea4d6226c3a57d9584edd1
cli <cli@DESKTOP-SVPJEK1>
2024-06-19 22:59:25 -0300

    concatenização de variaveis

---
+++ projeto2/src/aulas/Concatenização_variaveis.java
@@ -0,0 +1,19 @@
+package aulas;
+
+public class Concatenização_variaveis {
+	
+	public static void main(String[] args) {
+		
+		String nome = "Francisco Carvalho";
+		String cpf = "904.309.483-87";
+		String telefone = "86 9 98172-2795";
+		String endereco = "Timon - MA";
+		
+		System.out.println("Meu nome é:" + nome + 
+				           " e o cpf:" + cpf + 
+				           "meu contato:" + telefone +) 
+				           "meu endereço:" + endereco +);
+		
+	}
+
+}
--- projeto2/src/projeto2/Projeto2.java
+++
@@ -1,8 +0,0 @@
-package projeto2;
-
-public class Projeto2 {
-	
-	
-	/*código inteiro*/
-
-}
